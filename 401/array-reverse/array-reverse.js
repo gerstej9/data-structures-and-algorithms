d8c3f2bdea54518aabf0ce11aa1c5ab75f20d894
James Gerstenberger <gerstej9@gmail.com>
2021-02-15 14:20:38 -0800

    401 code challenge 01 array-reverse complete

---
+++ 401/array-reverse/array-reverse.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+const reverseArray = (arr) => {
+  let finlArr = []
+  for(let i = arr.length-1; i >= 0; i--){
+    finalArr.push(arr[i]);
+  }
+  return finalArr;
+}
+
+
+module.exports = reverseArray;
